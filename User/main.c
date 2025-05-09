3aee329b64d82d126b02a7d9b591082fd06d045e
luo <1493675790@qq.com>
2025-05-09 23:15:23 +0800

    终稿1.0

--- User/main.c
+++ User/main.c
@@ -33,7 +33,6 @@ int main()
 	OLED_Init();
 	Timer_Init();
 	Encoder_Init();
-	Serial_Init();
 	
 	Motor_Setspeed1(0);
 	Motor_Setspeed2(0);
@@ -44,36 +43,32 @@ int main()
 	Target3 = 10;
 	while(1)
 	{
-		Serial_Printf("%f,%f,%f\r\n",Target3,Actual3,Out3);
-		if(Location3 > 1500)Target3 = 20;
-		if(Location3 > 10000)Target3 = 40;
-		if(Location3 > 18000)Target3 = 20;
-//		if(abs(Location2 - current) <= 2*1500 + 50 && abs(Location2 - current) >= 2*1500 - 50 && state == 0){
-//			current = Location1;
-//			Target1 = 1.155/0.033;
-//			Target2 = 0;
-//			Target3 = -1.155/0.033;
-//			state++;
-//		}
-//		if(abs(Location1 - current) <= 2*1500 + 50 && abs(Location1 - current) >= 2*1500 - 50 && state == 1){
-//			current = Location2;
-//			Target1 = 0.5/0.033;
-//			Target2 = -1/0.033;
-//			Target3 = 0.5/0.033;
-//			state++;
-//		}
-//		if(abs(Location2 - current) <= 2*1500 + 50 && abs(Location2 - current) >= 2*1500 - 50 && state == 2){
-//			current = Location1;
-//			Target1 = -1.155/0.033;
-//			Target2 = 0;
-//			Target3 = 1.155/0.033;
-//			state++;
-//		}
-//		if(abs(Location1 - current) <= 2*1500 + 50 && abs(Location1 - current) >= 2*1500 - 50 && state == 3){
-//			Target1 = 0;
-//			Target2 = 0;
-//			Target3 = 0;
-//		}
+		if(abs(Location2 - current) <= 2*1500 + 50 && abs(Location2 - current) >= 2*1500 - 50 && state == 0){
+			current = Location1;
+			Target1 = 1.155/0.033;
+			Target2 = 0;
+			Target3 = -1.155/0.033;
+			state++;
+		}
+		if(abs(Location1 - current) <= 2*1500 + 50 && abs(Location1 - current) >= 2*1500 - 50 && state == 1){
+			current = Location2;
+			Target1 = 0.5/0.033;
+			Target2 = -1/0.033;
+			Target3 = 0.5/0.033;
+			state++;
+		}
+		if(abs(Location2 - current) <= 2*1500 + 50 && abs(Location2 - current) >= 2*1500 - 50 && state == 2){
+			current = Location1;
+			Target1 = -1.155/0.033;
+			Target2 = 0;
+			Target3 = 1.155/0.033;
+			state++;
+		}
+		if(abs(Location1 - current) <= 2*1500 + 50 && abs(Location1 - current) >= 2*1500 - 50 && state == 3){
+			Target1 = 0;
+			Target2 = 0;
+			Target3 = 0;
+		}
 		//}
 		OLED_ShowSignedNum(2,1,Actual1,5);
         OLED_ShowSignedNum(1,1,Location1,5);
--- hardware/Serial.c
+++ hardware/Serial.c
@@ -7,47 +7,31 @@ uint8_t Serial_RxFlag;
 
 void Serial_Init(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 	
 	USART_InitTypeDef USART_InitStructure;
 	USART_InitStructure.USART_BaudRate = 9600;
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx;
 	USART_InitStructure.USART_Parity = USART_Parity_No;
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_Init(USART1, &USART_InitStructure);
+	USART_Init(UART4, &USART_InitStructure);
 	
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	
-	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-	NVIC_Init(&NVIC_InitStructure);
-	
-	USART_Cmd(USART1, ENABLE);
+	USART_Cmd(UART4, ENABLE);
 }
 
 void Serial_SendByte(uint8_t Byte)
 {
-	USART_SendData(USART1, Byte);
-	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+	USART_SendData(UART4, Byte);
+	while (USART_GetFlagStatus(UART4, USART_FLAG_TXE) == RESET);
 }
 
 void Serial_SendArray(uint8_t *Array, uint16_t Length)
